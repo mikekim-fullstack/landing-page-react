e0a8cc735b77cd7a80102f7a183419c4b1bbbcd0
mikekim-fullstack <mikekim.fullstack@gmail.com>
2023-01-20 17:19:29 -0500

    initial setup

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import Sidebar from '../components/SideBar'
+import Navbar from '../components/NavBar'
+const Home = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+    return (
+        <>
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Navbar toggle={toggle} />
+        </>
+    )
+}
+
+export default Home
